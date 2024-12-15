ae8b0033f9de2472eddc9b23430855b382f7e6aa
Yousfi Abdelkader <95932250+sher0u@users.noreply.github.com>
2024-12-15 13:51:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello student "); // TO Print a text in the C#;
+        }
+    }
+}
+
